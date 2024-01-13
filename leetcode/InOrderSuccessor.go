33f92c76148bc479f5fda88bbbb2444fdc86f969
aqib javed <aqibbutt3078@gmail.com>
2024-01-13 01:02:03 +0000

    InOrderSuccessor of BST using golang iterative

--- leetcode/InOrderSuccessor.go
+++ leetcode/InOrderSuccessor.go
@@ -17,3 +17,18 @@ func find(root *TreeNode, p *TreeNode, successor *TreeNode) *TreeNode {
 		return find(root.Left, p, root)
 	}
 }
+
+// Time complexity OLog(N) or O(h)
+// Space complexity O(1)
+func findIterative(root *TreeNode, p *TreeNode) *TreeNode {
+	var successor *TreeNode = nil
+	for root != nil {
+		if p.Val >= root.Val {
+			root = root.Right
+		} else {
+			successor = root
+			root = root.Left
+		}
+	}
+	return successor
+}
