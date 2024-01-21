03451d9b41adc5c9265d833b4943157ced5de9de
aqib javed <aqibbutt3078@gmail.com>
2024-01-21 01:02:03 +0000

    Inorder traversal

---
+++ leetcode/bst/InOrderTraversal.go
@@ -0,0 +1,19 @@
+package bst
+
+func inorderTraversal(root *TreeNode) []int {
+	data := make([]int, 0, 10)
+	inorder(root, &data)
+	return data
+}
+
+// Time complexity O(n)
+// Space complexity OLog(n)
+func inorder(root *TreeNode, data *[]int) {
+	if root == nil {
+		return
+	}
+
+	inorder(root.Left, data)
+	*data = append(*data, root.Val)
+	inorder(root.Right, data)
+}
