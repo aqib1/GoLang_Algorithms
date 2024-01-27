63a8eb197882aa1a5fa790174f3355cd72111e3b
aqib javed <aqibbutt3078@gmail.com>
2024-01-27 01:02:03 +0000

    Level order traversal using golang

---
+++ leetcode/bst/LevelOrder.go
@@ -0,0 +1,24 @@
+package bst
+
+// Time complexity O(n)
+// Space complexity O(n)
+func levelOrder(root *TreeNode) [][]int {
+	data := make([][]int, 0, 10)
+
+	preorderSolve(root, &data, 0)
+
+	return data
+}
+
+func preorderSolve(root *TreeNode, data *[][]int, height int) {
+	if root == nil {
+		return
+	}
+	if len(*data) == height {
+		*data = append(*data, []int{})
+	}
+	(*data)[height] = append((*data)[height], root.Val)
+
+	preorderSolve(root.Left, data, height+1)
+	preorderSolve(root.Right, data, height+1)
+}
