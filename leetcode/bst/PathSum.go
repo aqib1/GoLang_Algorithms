4c7a50ef09453462bed620670579fb5f5e744a90
aqib javed <aqibbutt3078@gmail.com>
2024-02-05 01:02:03 +0400

    Path sum in binary tree

---
+++ leetcode/bst/PathSum.go
@@ -0,0 +1,18 @@
+package bst
+
+// Time complexity O(N)
+// Space complexity O(N)
+func hasPathSum(root *TreeNode, targetSum int) bool {
+	if root == nil {
+		return false
+	}
+
+	targetSum -= root.Val
+
+	if root.Left == nil && root.Right == nil && targetSum == 0 {
+		return true
+	}
+
+	return hasPathSum(root.Left, targetSum) ||
+		hasPathSum(root.Right, targetSum)
+}
