81ff5231e1320337a926a570d795bd64657e1a52
aqib-javed_jpmc <aqibbutt3078@gmail.com>
2024-04-23 17:07:23 +0100

    GoLang Slice

--- Slice.go
+++ Slice.go
@@ -1,1 +1,12 @@
 package main
+
+import "fmt"
+
+func main() {
+	var slice = []int{}
+	fmt.Println(slice)
+	slice = append(slice, 1)
+	fmt.Println(slice)
+	fmt.Println(cap(slice))
+	fmt.Println(len(slice))
+}
