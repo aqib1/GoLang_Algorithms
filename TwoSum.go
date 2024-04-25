14623a0e6d11f193dd948e5bd4e5ee537f3cca25
aqib-javed_jpmc <aqibbutt3078@gmail.com>
2024-04-25 03:11:01 +0100

    TwoSum leetcode algorithm optimal solution

--- TwoSum.go
+++ TwoSum.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum2([]int{2, 7, 11, 15}, 9))
 }
 
+// Time complexity O(n)2 & Space complexity O(2)
 func twoSum(nums []int, target int) []int {
 	var result []int
 	for i := 0; i < len(nums); i++ {
@@ -14,6 +15,23 @@ func twoSum(nums []int, target int) []int {
 				result = []int{i, j}
 				break
 			}
+		}
+	}
+
+	return result
+}
+
+// Time complexity O(n) and space O(n)
+func twoSum2(nums []int, target int) []int {
+	var result []int
+	var numberByIndex = make(map[int]int)
+
+	for i := 0; i < len(nums); i++ {
+		value, ok := numberByIndex[target-nums[i]]
+		if ok {
+			result = []int{value, i}
+		} else {
+			numberByIndex[nums[i]] = i
 		}
 	}
 
