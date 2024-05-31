07535897ebb57a86ac798220c4685c6ffed4f52b
aqib-javed_jpmc <aqibbutt3078@gmail.com>
2024-05-31 12:11:03 +0100

    Programming Fundamentals

--- programming_fundamentals/Primitives.go
+++ programming_fundamentals/Primitives.go
@@ -4,6 +4,12 @@ import "fmt"
 
 func main() {
 	var name string = "Aqib"
+	// := can not be used outside of function
+	name1 := "Aqib"
+	var name2 string
 
-	fmt.Println(name)
+	fmt.Println(name != name1)
+	fmt.Println(name2)
+	name2 = "Maria"
+	fmt.Println(name2)
 }
