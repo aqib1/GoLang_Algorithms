60e0fa711deb70655c11bc66efbb9a2831524e09
aqib javed <aqibbutt3078@gmail.com>
2025-01-10 00:21:57 +0000

    Find Lowest common ancestor using recursion and iterative approach

---
+++ leetcode/LowestCommonAncestorInBst.go
@@ -0,0 +1,39 @@
+package leetcode
+
+type TreeNode struct {
+	Val   int
+	Right *TreeNode
+	Left  *TreeNode
+}
+
+// Time complexity OLog(N)
+// Space complexity O(1)
+func lowestCommonAncestorIterative(root, p, q *TreeNode) *TreeNode {
+	for root != nil {
+		if p.Val > root.Val && q.Val > root.Val {
+			root = root.Right
+		} else if p.Val < root.Val && q.Val < root.Val {
+			root = root.Left
+		} else {
+			return root
+		}
+	}
+
+	return nil
+}
+
+// Time complexity OLog(N)
+// Space complexity OLog(N)
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}
+
+	return root
+}
